15fd178e6568d0c40481893ee805bb421722a97e
Frank Song <songfrank@hotmail.com>
2018-01-30 22:45:25 -0500

    resource page update

--- src/app/components/pages/resource-management/resource-management.component.ts
+++ src/app/components/pages/resource-management/resource-management.component.ts
@@ -6,11 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resource-management.component.scss']
 })
 export class ResourceManagementComponent implements OnInit {
+  screenText = {
+    id: '序号',
+    key: '资源索引键',
+    space: '　　',
+    createdBy: '建立人',
+    createdOn: '建立日期',
+    lastEditedOn: '编辑人',
+    lastEditedBy: '编辑日期'
+  };
+
+
   list = [
-    {id: '10000', editing: false, contentZH: 'this is a text content.', contentEN: 'this is a text content.', contentKey: 'TEXT-NEW-PAGE-A', createdOn: 'Dec 21, 2017', createdBy: 'Frank Song', lastEditedBy: 'Frank Song', lastEditedOn: 'Jan 1, 2018'},
-    {id: '2000', editing: false, contentZH: 'this is a text content.', contentEN: 'this is a text content.', contentKey: 'TEXT-NEW-PAGE-A', createdOn: 'Dec 21, 2017', createdBy: 'Frank Song', lastEditedBy: 'Frank Song', lastEditedOn: 'Jan 1, 2018'},
-    {id: '3', editing: false, contentZH: 'this is a text content.', contentEN: 'this is a text content.', contentKey: 'TEXT-NEW-PAGE-A', createdOn: 'Dec 21, 2017', createdBy: 'Frank Song', lastEditedBy: 'Frank Song', lastEditedOn: 'Jan 1, 2018'},
-    {id: '4', editing: false, contentZH: 'this is a text content.', contentEN: 'this is a text content.', contentKey: 'TEXT-NEW-PAGE-A', createdOn: 'Dec 21, 2017', createdBy: 'Frank Song', lastEditedBy: 'Frank Song', lastEditedOn: 'Jan 1, 2018'}
+    {id: '10000', editing: false, contentZH: 'this is a text content.', contentEN: 'this is a text content.', contentKey: 'TEXT-NEW-PAGE-A', createdOn: '2017-12-21', createdBy: 'Frank Song', lastEditedBy: 'Frank Song', lastEditedOn: '2017-12-21'},
+    {id: '2000', editing: false, contentZH: 'this is a text content.', contentEN: 'this is a text content.', contentKey: 'TEXT-NEW-PAGE-A', createdOn: '2017-12-21', createdBy: 'Frank Song', lastEditedBy: 'Frank Song', lastEditedOn: '2017-12-21'},
+    {id: '3', editing: false, contentZH: 'this is a text content.', contentEN: 'this is a text content.', contentKey: 'TEXT-NEW-PAGE-A', createdOn: '2017-12-21', createdBy: 'Frank Song', lastEditedBy: 'Frank Song', lastEditedOn: '2017-12-21'},
+    {id: '4', editing: false, contentZH: 'this is a text content.', contentEN: 'this is a text content.', contentKey: 'TEXT-NEW-PAGE-A', createdOn: '2017-12-21', createdBy: 'Frank Song', lastEditedBy: 'Frank Song', lastEditedOn: '2017-12-21'}
   ];
   constructor() { }
 
